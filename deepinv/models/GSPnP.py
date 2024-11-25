d67ad490652a24dc47cb484d4a78373b21bb60aa
Chicago PARK <73331241+ChicagoPark@users.noreply.github.com>
2024-11-25 00:38:36 -0600

    Add files via upload

---
+++ deepinv/models/GSPnP.py
@@ -0,0 +1,255 @@
+import torch
+import torch.nn as nn
+from .utils import get_weights_url
+from util.tweedie_utility import get_noiselevel_alphas_timestep, p_mean_variance
+
+class StudentGrad(nn.Module):
+    # ! HERE I MODIFY THE input argument
+    def __init__(self, denoiser, score=None):
+        super().__init__()
+        self.model = denoiser
+        self.score = score
+
+    def forward(self, x, sigma):
+        # print(f"sigma: {sigma}")
+        return self.model(x, sigma)
+    """
+    def forward(self, x, sigma):
+        gpu = 3
+        # device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
+        device_str = f"cuda:{gpu}" if torch.cuda.is_available() else 'cpu'
+        device = torch.device(device_str)
+        # raise ValueError(f"sigma: {sigma} / type:{type(sigma)}")
+        # sigma = sigma.cpu()
+        # print(f"sigma: {sigma}")
+        if isinstance(sigma, torch.Tensor):
+            sigma_float = float(sigma.item())
+            # raise ValueError()
+        else:
+            sigma_float = sigma
+        # time_corresponding_sigma_denoiser, alphas_corresponding_sigma_denoiser = get_noiselevel_alphas_timestep(0.06, beta_at_clean=0.0001, denoiser_network_type = "vp_score", num_diffusion_timesteps = 1000, last_time_step = 0)
+        time_corresponding_sigma_denoiser, alphas_corresponding_sigma_denoiser = get_noiselevel_alphas_timestep(sigma, beta_at_clean=0.0001, denoiser_network_type = "vp_score", num_diffusion_timesteps = 1000, last_time_step = 0)
+        time_corresponding_sigma_denoiser = torch.tensor([time_corresponding_sigma_denoiser] * x.shape[0], device=device)
+        alphas_corresponding_sigma_denoiser = torch.tensor(alphas_corresponding_sigma_denoiser)
+        print(f"time_corresponding_sigma_denoiser: {time_corresponding_sigma_denoiser}")
+        # raise ValueError(f"time_corresponding_sigma_denoiser: {time_corresponding_sigma_denoiser}\n sigma_denoiser: {sigma_denoiser}")
+        # model_dictionary = p_mean_variance(model = self.model, x = torch.sqrt(alphas_corresponding_sigma_denoiser) * (x), t = time_corresponding_sigma_denoiser, alphas = alphas_corresponding_sigma_denoiser)
+        model_dictionary = p_mean_variance(model = self.score, x = torch.sqrt(alphas_corresponding_sigma_denoiser) * (x), t = time_corresponding_sigma_denoiser, alphas = alphas_corresponding_sigma_denoiser)
+        model_output = model_dictionary['x_hat_MMSE']
+        
+        return model_output
+    """
+
+
+class GSPnP(nn.Module):
+    r"""
+    Gradient Step module to use a denoiser architecture as a Gradient Step Denoiser.
+    See https://arxiv.org/pdf/2110.03220.pdf.
+    Code from https://github.com/samuro95/GSPnP.
+
+    :param nn.Module denoiser: Denoiser model.
+    :param float alpha: Relaxation parameter
+    """
+
+    def __init__(self, denoiser, alpha=1.0):
+        super().__init__()
+        self.student_grad = StudentGrad(denoiser)
+        self.alpha = alpha
+        # ! HERE I EDIT THE LINE BELOW
+        self.denoiser = denoiser
+
+    def potential(self, x, sigma, *args, **kwargs):
+        N = self.student_grad(x, sigma)
+        # print(f"self.alpha: {self.alpha}")
+        return (
+            0.5
+            * self.alpha
+            * torch.norm((x - N).view(x.shape[0], -1), p=2, dim=-1) ** 2
+        )
+
+    def potential_grad(self, x, sigma, *args, **kwargs):
+        r"""
+        Calculate :math:`\nabla g` the gradient of the regularizer :math:`g` at input :math:`x`.
+
+        :param torch.Tensor x: Input image
+        :param float sigma: Denoiser level :math:`\sigma` (std)
+        """
+        # print(f"potential_grad: sigma {sigma}")
+        # raise ValueError()
+        with torch.enable_grad():
+            x = x.float()
+            x = x.requires_grad_()
+            N = self.student_grad(x, sigma)
+            JN = torch.autograd.grad(
+                N, x, grad_outputs=x - N, create_graph=True, only_inputs=True
+            )[0]
+        Dg = x - N - JN
+        return self.alpha * Dg
+
+    def forward(self, x, sigma):
+        r"""
+        Denoising with Gradient Step Denoiser
+
+        :param torch.Tensor x: Input image
+        :param float sigma: Denoiser level (std)
+        """
+        # raise ValueError(f"[GSPnP forward] sigma: {sigma}")
+        Dg = self.potential_grad(x, sigma)
+        x_hat = x - Dg
+        return x_hat
+
+
+class GSPnPScore(nn.Module):
+    r"""
+    Gradient Step module to use a denoiser architecture as a Gradient Step Denoiser.
+    See https://arxiv.org/pdf/2110.03220.pdf.
+    Code from https://github.com/samuro95/GSPnP.
+
+    :param nn.Module denoiser: Denoiser model.
+    :param float alpha: Relaxation parameter
+    """
+
+    def __init__(self, denoiser, score, alpha=1.0):
+        super().__init__()
+        self.student_grad = StudentGrad(denoiser, score)
+        self.alpha = alpha
+        # ! HERE I EDIT THE LINE BELOW
+        self.denoiser = denoiser
+        self.score = score
+
+    def potential(self, x, sigma, *args, **kwargs):
+        N = self.student_grad(x, sigma)
+        # print(f"self.alpha: {self.alpha}")
+        return (
+            0.5
+            * self.alpha
+            * torch.norm((x - N).view(x.shape[0], -1), p=2, dim=-1) ** 2
+        )
+
+    def potential_grad(self, x, sigma, *args, **kwargs):
+        r"""
+        Calculate :math:`\nabla g` the gradient of the regularizer :math:`g` at input :math:`x`.
+
+        :param torch.Tensor x: Input image
+        :param float sigma: Denoiser level :math:`\sigma` (std)
+        """
+        # print(f"potential_grad: sigma {sigma}")
+        # raise ValueError()
+        with torch.enable_grad():
+            x = x.float()
+            x = x.requires_grad_()
+            N = self.student_grad(x, sigma)
+            JN = torch.autograd.grad(
+                N, x, grad_outputs=x - N, create_graph=True, only_inputs=True
+            )[0]
+        Dg = x - N - JN
+        return self.alpha * Dg
+
+    def forward(self, x, sigma):
+        r"""
+        Denoising with Gradient Step Denoiser
+
+        :param torch.Tensor x: Input image
+        :param float sigma: Denoiser level (std)
+        """
+        # raise ValueError(f"[GSPnP forward] sigma: {sigma}")
+        Dg = self.potential_grad(x, sigma)
+        x_hat = x - Dg
+        return x_hat
+
+
+def GSDRUNet(
+    alpha=1.0,
+    in_channels=3,
+    out_channels=3,
+    nb=2,
+    nc=[64, 128, 256, 512],
+    act_mode="E",
+    pretrained=None,
+    device=torch.device("cpu"),
+):
+    """
+    Gradient Step Denoiser with DRUNet architecture
+
+    :param float alpha: Relaxation parameter
+    :param int in_channels: Number of input channels
+    :param int out_channels: Number of output channels
+    :param int nb: Number of blocks in the DRUNet
+    :param list nc: Number of channels in the DRUNet
+    :param str act_mode: activation mode, "R" for ReLU, "L" for LeakyReLU "E" for ELU and "S" for Softplus.
+    :param str downsample_mode: Downsampling mode, "avgpool" for average pooling, "maxpool" for max pooling, and
+        "strideconv" for convolution with stride 2.
+    :param str upsample_mode: Upsampling mode, "convtranspose" for convolution transpose, "pixelsuffle" for pixel
+        shuffling, and "upconv" for nearest neighbour upsampling with additional convolution.
+    :param bool download: use a pretrained network. If ``pretrained=None``, the weights will be initialized at random
+        using Pytorch's default initialization. If ``pretrained='download'``, the weights will be downloaded from an
+        online repository (only available for the default architecture).
+        Finally, ``pretrained`` can also be set as a path to the user's own pretrained weights.
+        See :ref:`pretrained-weights <pretrained-weights>` for more details.
+    :param bool train: training or testing mode.
+    :param str device: gpu or cpu.
+
+    """
+    from deepinv.models.drunet import DRUNet
+
+    denoiser = DRUNet(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        nb=nb,
+        nc=nc,
+        act_mode=act_mode,
+        pretrained=None,
+        device=device,
+    )
+    GSmodel = GSPnP(denoiser, alpha=alpha)
+    if pretrained:
+        if pretrained == "download":
+            url = get_weights_url(
+                model_name="gradientstep", file_name="GSDRUNet_torch.ckpt"
+            )
+            ckpt = torch.hub.load_state_dict_from_url(
+                url,
+                map_location=lambda storage, loc: storage,
+                file_name="GSDRUNet_torch.ckpt",
+            )
+        else:
+            ckpt = torch.load(pretrained, map_location=lambda storage, loc: storage)
+
+        if "state_dict" in ckpt:
+            ckpt = ckpt["state_dict"]
+
+        GSmodel.load_state_dict(ckpt, strict=False)
+        GSmodel.eval()
+    return GSmodel
+
+
+def GSScore(
+    score,
+    alpha=1.0,
+    device=torch.device("cpu"),
+):
+    """
+    Gradient Step Denoiser with DRUNet architecture
+
+    :param float alpha: Relaxation parameter
+    :param int in_channels: Number of input channels
+    :param int out_channels: Number of output channels
+    :param int nb: Number of blocks in the DRUNet
+    :param list nc: Number of channels in the DRUNet
+    :param str act_mode: activation mode, "R" for ReLU, "L" for LeakyReLU "E" for ELU and "S" for Softplus.
+    :param str downsample_mode: Downsampling mode, "avgpool" for average pooling, "maxpool" for max pooling, and
+        "strideconv" for convolution with stride 2.
+    :param str upsample_mode: Upsampling mode, "convtranspose" for convolution transpose, "pixelsuffle" for pixel
+        shuffling, and "upconv" for nearest neighbour upsampling with additional convolution.
+    :param bool download: use a pretrained network. If ``pretrained=None``, the weights will be initialized at random
+        using Pytorch's default initialization. If ``pretrained='download'``, the weights will be downloaded from an
+        online repository (only available for the default architecture).
+        Finally, ``pretrained`` can also be set as a path to the user's own pretrained weights.
+        See :ref:`pretrained-weights <pretrained-weights>` for more details.
+    :param bool train: training or testing mode.
+    :param str device: gpu or cpu.
+
+    """
+    GSmodel = GSPnP(score, alpha=alpha)
+    GSmodel.eval()
+    return GSmodel
---
+++ deepinv/models/__init__.py
@@ -0,0 +1,31 @@
+from .drunet import DRUNet
+from .scunet import SCUNet
+from .ae import AutoEncoder
+from .unet import UNet
+from .dncnn import DnCNN
+from .artifactremoval import ArtifactRemoval
+from .tv import TVDenoiser
+from .tgv import TGVDenoiser
+from .wavdict import WaveletDenoiser, WaveletDictDenoiser
+# ! HERE I modify the module importing 
+# from .GSPnP import GSDRUNet
+from .GSPnP import GSDRUNet, GSScore, GSPnPScore
+from .median import MedianFilter
+from .dip import DeepImagePrior, ConvDecoder
+from .diffunet import DiffUNet
+from .swinir import SwinIR
+from .PDNet import PDNet_PrimalBlock, PDNet_DualBlock
+from .bm3d import BM3D
+from .equivariant import EquivariantDenoiser
+from .epll import EPLLDenoiser
+from .restormer import Restormer
+from .icnn import ICNN
+from .gan import (
+    PatchGANDiscriminator,
+    ESRGANDiscriminator,
+    CSGMGenerator,
+    DCGANGenerator,
+    DCGANDiscriminator,
+)
+from .complex import to_complex_denoiser
+from .dynamic import TimeAgnosticNet, TimeAveragingNet
